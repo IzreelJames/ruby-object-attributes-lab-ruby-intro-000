bf0baa676690ff43a46dc80226d14251255986bb
Izreel James <izreeljames@gmail.com>
2019-07-04 23:58:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,8 +12,8 @@ class Dog
   
 end
   
-lassie.instance_variable_set=( :@name,"lassie" )
-lassie.instace_variable_get = (:@name)
+lassie.instance_variable_set( :@name,"lassie" )
+lassie.instace_variable_get(:@name)
 
 fido = Dog.new("fido")
 fido.name 
